07894c8fc69a10198aa3a9932a49b2fc82dfa37c
adityagnayak <47325880+adityagnayak@users.noreply.github.com>
2026-02-07 20:20:02 +0000

    Update retirement_webpage.py

--- retirement_webpage.py
+++ retirement_webpage.py
@@ -4,8 +4,7 @@
   "metadata": {
     "colab": {
       "provenance": [],
-      "authorship_tag": "ABX9TyPD3sm6JRu22LkH+jsaMYGl",
-      "include_colab_link": true
+      "authorship_tag": "ABX9TyPD3sm6JRu22LkH+jsaMYGl"
     },
     "kernelspec": {
       "name": "python3",
